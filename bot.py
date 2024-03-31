ad5646b2eb7ec173fc11d1b5251c0c0bead7cb54
groundsc <groundsc@student.21-school.ru>
2024-04-01 00:25:20 +0300

    dataset of Sevda

---
+++ bot.py
@@ -0,0 +1,18 @@
+from PIL import Image
+from PIL import ImageFilter
+from rembg import remove
+import os
+# image_filename="img1"
+for i in range(1, 51):
+    image_filename = f'img{i}'
+    img = Image.open(f"{image_filename}.jpg") #открыли изображение
+    img=remove(img) #удалили фон
+    img.thumbnail((100, 100), Image.NEAREST) #уменьшили разрешение
+    img.save(f"images/{image_filename}_1.png") #сохранили
+    img2 = img.filter(ImageFilter.SHARPEN)
+    img2.save(f"images/{image_filename}_2.png")
+    img2=img.transpose(Image.FLIP_LEFT_RIGHT) #развернули
+    img2.save(f"images/{image_filename}_3.png") #сохранили
+    img2 = img2.filter(ImageFilter.DETAIL)
+    img2.save(f"images/{image_filename}_4.png")
+    os.remove(f"{image_filename}.jpg") #удалили исходный
